2fd9ff63e1174059a7157079f0ab450aad5f5b3c
istoric564 <mrdizzy38@gmail.com>
2021-05-08 22:54:35 +0700

    Added SessionServlet

---
+++ src/http/servlet/SessionServlet.java
@@ -0,0 +1,22 @@
+package http.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebServlet("/sessions")
+public class SessionServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        var session = req.getSession();
+        System.out.println(session.isNew());
+    }
+}
+
+
